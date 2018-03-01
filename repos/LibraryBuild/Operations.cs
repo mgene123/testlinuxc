a8597e00cfe1cc8863092902b6ae302a30f8e302
Mohamed <motaz@mohamed.local>
2018-03-01 05:45:26 -0800

    initial commit

---
+++ repos/LibraryBuild/Operations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBuild
+{
+    public class Operations
+    {
+        public double Sum(double num1 ,double num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
